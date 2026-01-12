049280d2fd190d12ea28f170c3036b3f15915922
mubarak23 <mubarakaminu340@gmail.com>
2026-01-12 09:05:43 +0100

    chore:test endpoint and fix build changes

--- breezeln/src/routes/invoice.rs
+++ breezeln/src/routes/invoice.rs
@@ -1,5 +1,5 @@
 use axum::{extract::State, Json};
-use breez_sdk_spark::{ReceivePaymentRequest, ReceivePaymentMethod, ListPaymentsRequest};
+use breez_sdk_spark::{ReceivePaymentRequest, ReceivePaymentMethod, ListPaymentsRequest, Payment};
 use serde::{Deserialize, Serialize};
 use crate::state::AppState;
 
@@ -20,52 +20,6 @@ pub struct PaymentListsResponse {
 }
 
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum PaymentType {
-    Sent,
-    Received
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum PaymentStatus {
-    Pending,
-    Complete,
-    Failed,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum PaymentMethod {
-    Lightning,
-    Onchain,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct LightningPaymentDetails {
-  pub description: Option<String>,
-  pub primage: Option<String>,
-  pub invoice: String,
-  pub payment_hash: String,
-  pub destination_pubkey: String,
-  pub lnurl_pay_info: Option<serde_json::Value>,
-  pub lnurl_withdraw_info: Option<serde_json::Value>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Payment {
-    pub id: String,
-    pub payment_type: PaymentType,
-    pub status: PaymentStatus,
-    pub amount: u64,     
-    pub fees: u64,        
-    pub timestamp: i64,   
-    pub method: PaymentMethod,
-    pub details: Option<LightningPaymentDetails>,
-}
-
-
-
-
-
 pub async fn create_invoice(
    State(state): State<AppState>,
     Json(req): Json<CreateInvoiceRequest>,
@@ -73,23 +27,23 @@ pub async fn create_invoice(
   let response = state.breeze.receive_payment(
       ReceivePaymentRequest {
         payment_method: ReceivePaymentMethod::Bolt11Invoice {
-          description: req.description,
-          amount_sats: req.amount_sats
+          description: req.description.unwrap(),
+          amount_sats: Some(req.amount_sats)
         }
       }
-  ).await?;
+  ).await;
   
   Json(CreateInvoiceResponse {
-        paymentRequest: response.payment_request,
+        paymentRequest: response.unwrap().payment_request,
     })
 }
 
 pub async fn list_payments(
   State(state): State<AppState>
 ) -> Json<PaymentListsResponse> {
-  let response state.breeze.list_payments(ListPaymentsRequest::default()).await();
+  let response = state.breeze.list_payments(ListPaymentsRequest::default()).await;
 
   Json(PaymentListsResponse {
-    paymentListsResponse: response.payments 
+    paymentListsResponse: response.unwrap().payments 
   })
 }
